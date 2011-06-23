f861035a4cbc404d577dfbaf975d26cb15490981
Guillaume de Bure <guillaume.debure@gmail.com>
2011-06-23 18:31:59 +0200

    Use a char(1) field for id

--- users/models.py
+++ users/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Profile (models.Model):
+    id = models.CharField(max_length=1,primary_key=True)
     name = models.CharField(max_length=32)
     description = models.CharField(max_length=255)
