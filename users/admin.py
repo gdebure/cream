662add015488e5e6be9697ab2147432ea34d99b9
Guillaume de Bure <guillaume.debure@gmail.com>
2011-06-24 17:53:41 +0200

    Fix users model

---
+++ users/admin.py
@@ -0,0 +1,5 @@
+from users.models import Profile, Employee
+from django.contrib import admin
+
+admin.site.register(Profile)
+admin.site.register(Employee)
--- users/models.py
+++ users/models.py
@@ -1,7 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 class Profile (models.Model):
+    '''A Class to handle user profiles'''
+    
     id = models.CharField(max_length=1,primary_key=True)
     name = models.CharField(max_length=32)
     description = models.CharField(max_length=255)
+    
+    def __unicode__(self):
+        return self.name
+    
+    
+    
+class Employee (models.Model):
+    '''A class to handle employees'''
+    
+    user = models.ForeignKey(User, unique=True, null=True)
+    siglum = models.CharField(max_length=16)
+    
+    def __unicode__(self):
+        return self.user.last_name + " " + self.user.first_name
+    
+    
+    
