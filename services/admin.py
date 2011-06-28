fdd17fe35a72b8382723d2edc21f8ef7f2989d6f
Guillaume de Bure <guillaume.debure@gmail.com>
2011-06-28 13:57:06 +0200

    Domains administration

---
+++ services/admin.py
@@ -0,0 +1,4 @@
+from services.models import Domain
+from django.contrib import admin
+
+admin.site.register(Domain)
--- services/models.py
+++ services/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from users.models import User
 
-class domain (models.Model):
+class Domain (models.Model):
     
     name = models.CharField(max_length=64)
     is_active = models.BooleanField()
     owner = models.ForeignKey(User)
     description = models.TextField(null=True)
+
+    def __unicode__(self):
+        return self.name
