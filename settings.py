b1912780983ab1c7a4e594ff8281e74d94c56061
Guillaume DE BURE <guillaume.debure@airbus.com>
2011-07-22 10:26:27 +0200

    Use a portable path for templates

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
 # Django settings for CREAM project.
 
+import os
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -106,7 +108,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/guillaume/Developpement/git/CREAM/templates"
+    os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
 )
 
 INSTALLED_APPS = (
