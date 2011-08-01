a1d1e14cafd3fb1fccfffda171f4eda4ee61dae4
Guillaume DE BURE <guillaume.debure@airbus.com>
2011-08-01 14:25:41 +0200

    Skill categories

--- qualifications/forms.py
+++ qualifications/forms.py
@@ -1,8 +1,13 @@
 from django import forms
 
-from qualifications.models import Skill
+from qualifications.models import Skill, SkillCategory
 
 class SkillForm(forms.ModelForm):
     class Meta:
         model = Skill
+        
+
+class SkillCategoryForm(forms.ModelForm):
+    class Meta:
+        model = SkillCategory
         
--- qualifications/urls.py
+++ qualifications/urls.py
@@ -1,16 +1,23 @@
 from django.conf.urls.defaults import *
 from django.views.generic import DetailView, ListView, UpdateView, CreateView
-from qualifications.models import Skill
+from qualifications.models import Skill, SkillCategory
 from qualifications.forms import SkillForm
 
 urlpatterns = patterns('',
     ##################################
     # Skills 
-    (r'^skills/$', ListView.as_view( queryset=Skill.objects.order_by('name'), context_object_name='skills_list', ), ),
+    (r'^skills/$', ListView.as_view( queryset=Skill.objects.order_by('category','name'), context_object_name='skills_list', ), ),
     (r'^skills/(?P<pk>\d+)/$', DetailView.as_view( model=Skill, ), ),
     (r'^skills/(?P<pk>\d+)/update/$', UpdateView.as_view( model=Skill, success_url='/qualifications/skills/%(id)s' ), ),
     (r'^skills/create/$', CreateView.as_view( model=Skill, success_url='/qualifications/skills/%(id)s' ), ),
     ##################################
     
+    ##################################
+    # Skill Categories 
+    (r'^skill_categories/$', ListView.as_view( queryset=SkillCategory.objects.order_by('name'), context_object_name='skill_categories_list', ), ),
+    (r'^skill_categories/(?P<pk>\d+)/$', DetailView.as_view( model=SkillCategory, ), ),
+    (r'^skill_categories/(?P<pk>\d+)/update/$', UpdateView.as_view( model=SkillCategory, success_url='/qualifications/skill_categories/%(id)s' ), ),
+    (r'^skill_categories/create/$', CreateView.as_view( model=SkillCategory, success_url='/qualifications/skill_categories/%(id)s' ), ),
+    ##################################
     
 )
