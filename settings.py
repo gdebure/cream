b31ecfc9cac0de48bf49ad5d5527fe842c6a818d
Guillaume DE BURE <guillaume.debure@airbus.com>
2011-08-04 11:18:50 +0200

    Handle user login

--- settings.py
+++ settings.py
@@ -172,3 +172,5 @@ TEMPLATE_CONTEXT_PROCESSORS = (
      'django.contrib.auth.context_processors.auth',
      'richtemplates.context_processors.media',
 )
+
+LOGIN_URL="/users/login/"
--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,8 @@ from django.views.generic import DetailView, ListView, UpdateView, CreateView
 from users.models import Employee
 
 urlpatterns = patterns('',
+    (r'^login/', 'django.contrib.auth.views.login', {'template_name': 'users/login.html'}),
     (r'^employees/$', ListView.as_view( queryset=Employee.objects.order_by('id'), context_object_name='employees_list', ),),
     (r'^employees/(?P<pk>\d+)/$', DetailView.as_view( model=Employee, ),),
+    
 )
