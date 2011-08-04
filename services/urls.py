adcd9ad42ba5fd0de08285f7545e5fd928583052
Guillaume DE BURE <guillaume.debure@airbus.com>
2011-08-04 11:19:09 +0200

    Protect access to domains

--- services/urls.py
+++ services/urls.py
@@ -1,15 +1,17 @@
 from django.conf.urls.defaults import *
+from django.contrib.auth.decorators import permission_required, login_required
+
 from django.views.generic import DetailView, ListView, UpdateView, CreateView, DeleteView
 from services.models import Domain, ServiceFamily, Service
 
 urlpatterns = patterns('',
     ##################################
     # Domains 
-    (r'^domains/$', ListView.as_view( queryset=Domain.objects.order_by('name'), context_object_name='domains_list', ), ),
-    (r'^domains/(?P<pk>\d+)/$', DetailView.as_view( model=Domain, ), ),
-    (r'^domains/create/$', CreateView.as_view( model=Domain, success_url='/services/domains/%(id)s' ), ),
-    (r'^domains/(?P<pk>\d+)/update/$', UpdateView.as_view( model=Domain, success_url='/services/domains/%(id)s' ), ),
-    (r'^domains/(?P<pk>\d+)/delete/$', DeleteView.as_view( model=Domain, success_url='/services/domains/' ), ),
+    (r'^domains/$', login_required()(ListView.as_view( queryset=Domain.objects.order_by('name'), context_object_name='domains_list', )), ),
+    (r'^domains/(?P<pk>\d+)/$', login_required()(DetailView.as_view( model=Domain, )), ),
+    (r'^domains/create/$', permission_required('services.add_domain')(CreateView.as_view( model=Domain, success_url='/services/domains/%(id)s' )), ),
+    (r'^domains/(?P<pk>\d+)/update/$', permission_required('services.change_domain')(UpdateView.as_view( model=Domain, success_url='/services/domains/%(id)s' )), ),
+    (r'^domains/(?P<pk>\d+)/delete/$', permission_required('services.delete_domain')(DeleteView.as_view( model=Domain, success_url='/services/domains/' )), ),
     ##################################
     
     ##################################
