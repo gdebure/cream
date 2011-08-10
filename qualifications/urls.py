7c2f748b1388c7f79666a1915a64d92470922800
Guillaume DE BURE <guillaume.debure@airbus.com>
2011-08-10 16:02:55 +0200

    Use ordering defined in models in generic views

--- qualifications/urls.py
+++ qualifications/urls.py
@@ -8,7 +8,7 @@ from qualifications.forms import SkillForm
 urlpatterns = patterns('',
     ##################################
     # Skills 
-    (r'^skills/$', login_required()(ListView.as_view( queryset=Skill.objects.order_by('category','name'), context_object_name='skills_list', )), ),
+    (r'^skills/$', login_required()(ListView.as_view( model=Skill, context_object_name='skills_list', )), ),
     (r'^skills/(?P<pk>\d+)/$', login_required()(DetailView.as_view( model=Skill, )), ),
     (r'^skills/create/$', permission_required('qualifications.add_skill')(CreateView.as_view( model=Skill, success_url='/qualifications/skills/%(id)s' )), ),
     (r'^skills/(?P<pk>\d+)/update/$', permission_required('qualifications.change_skill')(UpdateView.as_view( model=Skill, success_url='/qualifications/skills/%(id)s' )), ),
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
     
     ##################################
     # Skill Categories 
-    (r'^skill_categories/$', login_required()(ListView.as_view( queryset=SkillCategory.objects.order_by('name'), context_object_name='skill_categories_list', )), ),
+    (r'^skill_categories/$', login_required()(ListView.as_view( model=SkillCategory, context_object_name='skill_categories_list', )), ),
     (r'^skill_categories/(?P<pk>\d+)/$', login_required()(DetailView.as_view( model=SkillCategory, )), ),
     (r'^skill_categories/create/$', permission_required('qualifications.add_skillcategory')(CreateView.as_view( model=SkillCategory, success_url='/qualifications/skill_categories/%(id)s' )), ),
     (r'^skill_categories/(?P<pk>\d+)/update/$', permission_required('qualifications.change_skillcategory')(UpdateView.as_view( model=SkillCategory, success_url='/qualifications/skill_categories/%(id)s' )), ),
@@ -26,7 +26,7 @@ urlpatterns = patterns('',
     
     ##################################
     # Jobs
-    (r'^jobs/$', login_required()(ListView.as_view( queryset=Job.objects.order_by('name'), context_object_name='jobs_list', )), ),
+    (r'^jobs/$', login_required()(ListView.as_view( model=Job, context_object_name='jobs_list', )), ),
     (r'^jobs/(?P<pk>\d+)/$', login_required()(DetailView.as_view( model=Job, )), ),
     (r'^jobs/create/$', permission_required('qualifications.add_job')(CreateView.as_view( model=Job, success_url='/qualifications/jobs/%(id)s' )), ),
     (r'^jobs/(?P<pk>\d+)/update/$', permission_required('qualifications.change_job')(UpdateView.as_view( model=Job, success_url='/qualifications/jobs/%(id)s' )), ),
@@ -35,7 +35,7 @@ urlpatterns = patterns('',
     
     ##################################
     # Employee Skills 
-    (r'^employee_skills/$', login_required()(ListView.as_view( queryset=EmployeeSkill.objects.order_by('employee','skill'), context_object_name='employeeskills_list', )), ),
+    (r'^employee_skills/$', login_required()(ListView.as_view( model=EmployeeSkill, context_object_name='employeeskills_list', )), ),
     (r'^employee_skills/(?P<pk>\d+)/$', login_required()(DetailView.as_view( model=EmployeeSkill, )), ),
     (r'^employee_skills/create/$', permission_required('qualifications.add_employeeskill')(CreateView.as_view( model=EmployeeSkill, success_url='/qualifications/employee_skills/%(id)s' )), ),
     (r'^employee_skills/(?P<pk>\d+)/update/$', permission_required('qualifications.change_employeeskill')(UpdateView.as_view( model=EmployeeSkill, success_url='/qualifications/employee_skills/%(id)s' )), ),
@@ -44,7 +44,7 @@ urlpatterns = patterns('',
     
     ##################################
     # profiles 
-    (r'^profiles/$', login_required()(ListView.as_view( queryset=Profile.objects.order_by('name'), context_object_name='profiles_list', )), ),
+    (r'^profiles/$', login_required()(ListView.as_view( model=Profile, context_object_name='profiles_list', )), ),
     (r'^profiles/(?P<pk>\d+)/$', login_required()(DetailView.as_view( model=Profile, )), ),
     (r'^profiles/create/$', permission_required('qualifications.add_profile')(CreateView.as_view( model=Profile, success_url='/qualifications/profiles/%(id)s' )), ),
     (r'^profiles/(?P<pk>\d+)/update/$', permission_required('qualifications.change_profile')(UpdateView.as_view( model=Profile, success_url='/qualifications/profiles/%(id)s' )), ),
@@ -53,7 +53,7 @@ urlpatterns = patterns('',
     
     ##################################
     # job profile Skills 
-    (r'^job_skills/$', login_required()(ListView.as_view( queryset=JobProfileSkill.objects.order_by('job','skill'), context_object_name='jobprofileskills_list', )), ),
+    (r'^job_skills/$', login_required()(ListView.as_view( model=JobProfileSkill, context_object_name='jobprofileskills_list', )), ),
     (r'^job_skills/(?P<pk>\d+)/$', login_required()(DetailView.as_view( model=JobProfileSkill, )), ),
     (r'^job_skills/create/$', permission_required('qualifications.add_jobprofileskill')(CreateView.as_view( model=JobProfileSkill, success_url='/qualifications/job_skills/%(id)s' )), ),
     (r'^job_skills/(?P<pk>\d+)/update/$', permission_required('qualifications.add_jobprofileskill')(UpdateView.as_view( model=JobProfileSkill, success_url='/qualifications/job_skills/%(id)s' )), ),
