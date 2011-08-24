fdaa42eab80e9d90ea304ae8cca7bdbb3fec99ac
Guillaume DE BURE <guillaume.debure@airbus.com>
2011-08-24 10:14:15 +0200

    Allow some non mandatory fields for projects

--- projects/models.py
+++ projects/models.py
@@ -11,11 +11,11 @@ class Project (models.Model):
     name = models.CharField(max_length=64)
     number = models.CharField(max_length=32, unique=True)
     description = models.TextField()
-    date_start = models.DateField()
-    date_end = models.DateField()
-    customer_name = models.CharField(max_length=128, null=True)
-    customer_siglum = models.CharField(max_length=16, null=True)
-    wiki_link = models.URLField(null=True)
+    date_start = models.DateField(null=True, blank=True)
+    date_end = models.DateField(null=True, blank=True)
+    customer_name = models.CharField(max_length=128, null=True, blank=True)
+    customer_siglum = models.CharField(max_length=16, null=True, blank=True)
+    wiki_link = models.URLField(null=True, blank=True)
     
     def __unicode__(self):
         return self.number + ": " + self.name
