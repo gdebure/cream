c89ae627e74d9ffcb58994da1c780d810fc2ec29
Guillaume DE BURE <guillaume.debure@airbus.com>
2011-08-26 16:36:09 +0200

    Fix per object rights

--- services/urls.py
+++ services/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls.defaults import *
 from django.contrib.auth.decorators import permission_required, login_required
 
-from django.views.generic import DetailView, ListView, UpdateView, CreateView, DeleteView
+from django.views.generic import DetailView, ListView, CreateView, DeleteView
 from services.models import Domain, ServiceFamily, Service
 from services.forms import DomainForm, ServiceFamilyForm, ServiceForm, AddServiceFamilyForm
 
-from services.views import DomainUpdateView
+from services.views import DomainUpdateView, ServiceFamilyUpdateView, ServiceUpdateView
 
 urlpatterns = patterns('',
     ##################################
@@ -13,7 +13,7 @@ urlpatterns = patterns('',
     (r'^domains/$', login_required()(ListView.as_view( model=Domain, context_object_name='domains_list', )), ),
     (r'^domains/(?P<pk>\d+)/$', login_required()(DetailView.as_view( model=Domain, )), ),
     (r'^domains/create/$', permission_required('services.add_domain')(CreateView.as_view( model=Domain, form_class=DomainForm, success_url='/services/domains/%(id)s' )), ),
-    (r'^domains/(?P<pk>\d+)/update/$', permission_required('services.delete_domain')(UpdateView.as_view( model=Domain, form_class=DomainForm, success_url='/services/domains/%(id)s' )), ),
+    (r'^domains/(?P<pk>\d+)/update/$', DomainUpdateView.as_view( model=Domain, form_class=DomainForm, success_url='/services/domains/%(id)s' ), ),
     (r'^domains/(?P<pk>\d+)/delete/$', permission_required('services.delete_domain')(DeleteView.as_view( model=Domain, success_url='/services/domains/' )), ),
     #(r'^domains/(?P<pk>\d+)/add_servicefamily/$', permission_required('services.change_domain')(CreateView.as_view( model=ServiceFamily, form_class=AddServiceFamilyForm,  success_url='/services/domains/%(id)s' )), ),
     ##################################
@@ -23,7 +23,7 @@ urlpatterns = patterns('',
     (r'^service_families/$', login_required()(ListView.as_view( model=ServiceFamily, context_object_name='servicefamilies_list', )), ),
     (r'^service_families/(?P<pk>\d+)/$', login_required()(DetailView.as_view( model=ServiceFamily, )), ),
     (r'^service_families/create/$', permission_required('services.add_servicefamily')(CreateView.as_view( model=ServiceFamily, form_class=ServiceFamilyForm, success_url='/services/service_families/%(id)s' )), ),
-    (r'^service_families/(?P<pk>\d+)/update/$', permission_required('services.change_servicefamily')(UpdateView.as_view( model=ServiceFamily, form_class=ServiceFamilyForm, success_url='/services/service_families/%(id)s' )), ),
+    (r'^service_families/(?P<pk>\d+)/update/$', ServiceFamilyUpdateView.as_view( model=ServiceFamily, form_class=ServiceFamilyForm, success_url='/services/service_families/%(id)s' ), ),
     (r'^service_families/(?P<pk>\d+)/delete/$', permission_required('services.delete_servicefamily')(DeleteView.as_view( model=ServiceFamily, success_url='/services/service_families/' )), ),
     ##################################
     
@@ -32,7 +32,7 @@ urlpatterns = patterns('',
     (r'^services/$', login_required()(ListView.as_view( model=Service, context_object_name='services_list', )), ),
     (r'^services/(?P<pk>\d+)/$', login_required()(DetailView.as_view( model=Service, )), ),
     (r'^services/create/$', permission_required('services.add_service')(CreateView.as_view( model=Service, form_class=ServiceForm, success_url='/services/services/%(id)s' )), ),
-    (r'^services/(?P<pk>\d+)/update/$', permission_required('services.change_service')(UpdateView.as_view( model=Service, form_class=ServiceForm, success_url='/services/services/%(id)s' )), ),
+    (r'^services/(?P<pk>\d+)/update/$', ServiceUpdateView.as_view( model=Service, form_class=ServiceForm, form_class=ServiceForm, success_url='/services/services/%(id)s' ), ),
     (r'^services/(?P<pk>\d+)/delete/$', permission_required('services.delete_service')(DeleteView.as_view( model=Service, success_url='/services/services/' )), ),
     ##################################
     
--- services/views.py
+++ services/views.py
@@ -2,7 +2,7 @@ from django.views.generic import UpdateView
 from guardian.decorators import permission_required
 from django.utils.decorators import method_decorator
 
-from services.models import Domain
+from services.models import Domain, ServiceFamily, Service
 
 class DomainUpdateView(UpdateView):
 
@@ -10,6 +10,17 @@ class DomainUpdateView(UpdateView):
     def dispatch(self, *args, **kwargs):
         return super(DomainUpdateView, self).dispatch(*args, **kwargs)
 
+class ServiceFamilyUpdateView(UpdateView):
+
+    @method_decorator(permission_required('services.change_servicefamily',(ServiceFamily, 'id', 'pk')))
+    def dispatch(self, *args, **kwargs):
+        return super(ServiceFamilyUpdateView, self).dispatch(*args, **kwargs)
+
+class ServiceUpdateView(UpdateView):
+
+    @method_decorator(permission_required('services.change_service',(Service, 'id', 'pk')))
+    def dispatch(self, *args, **kwargs):
+        return super(ServiceUpdateView, self).dispatch(*args, **kwargs)
 
 #class AddServiceFamilyView(UpdateView):
 
