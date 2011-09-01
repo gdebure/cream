8e5894063605247870c02efe85c2087debb16f64
unknown <ST01447@.tls.fr.eu.airbus.corp>
2011-09-01 18:17:43 +0200

    Dummy report

--- services/views.py
+++ services/views.py
@@ -36,11 +36,38 @@ def domains_report(request):
     
     domains = Domain.objects.all()
     response = "<h1>Domain Reporting</h1><hr/>"
+    
     for domain in domains:
+        domain_turnover = 0
         response += "Domain: " + str(domain) + "<br/>"
         families = domain.servicefamily_set.all()
-        response += "Services Families: " + str(families.count())
-        response += "<hr/>"
+    
+        for family in families:
+            family_turnover = 0
+            response += "Service Family: " + str(family) + "<br/>"
+            services = family.service_set.all()
+            
+            for service in services:
+                service_turnover = 0
+                response += "Service: " + str(service) + "<br/>"
+                deliverables = service.deliverable_set.all()
+                
+                for deliverable in deliverables:
+                    deliverable_turnover = 0
+                    if deliverable.contractual_volume != None and deliverable.unit_price != None:
+                        deliverable_turnover = deliverable.contractual_volume * deliverable.unit_price
+                        response += "Deliverable: " + str(deliverable) + " = " + str(deliverable_turnover) + "<br/>"
+                    service_turnover += deliverable_turnover
+                
+                response += "Service Total = " + str(service_turnover) + "</br>"
+                family_turnover += service_turnover
+            
+            response += "Family Total = " + str(family_turnover) + "</br>"
+            domain_turnover += family_turnover
+        
+        response += "Domain Total = " + str(domain_turnover) + "</br>"
+                
     
     return HttpResponse(response)
         
+
