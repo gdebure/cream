1b0ddd731fbd9e3a3a49c2d65f00bdefafa4d32e
Guillaume de Bure <guillaume.debure@gmail.com>
2011-10-17 17:57:18 +0200

    Add a number of units on tasks

---
+++ projects/migrations/0021_auto__add_field_task_number_of_units__chg_field_task_time_spent__chg_f.py
@@ -0,0 +1,191 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'Task.number_of_units'
+        db.add_column('projects_task', 'number_of_units', self.gf('django.db.models.fields.IntegerField')(default=1), keep_default=False)
+
+        # Changing field 'Task.time_spent'
+        db.alter_column('projects_task', 'time_spent', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=5, decimal_places=2))
+
+        # Changing field 'Task.reject_reason'
+        db.alter_column('projects_task', 'reject_reason', self.gf('django.db.models.fields.CharField')(max_length=64, null=True))
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'Task.number_of_units'
+        db.delete_column('projects_task', 'number_of_units')
+
+        # Changing field 'Task.time_spent'
+        db.alter_column('projects_task', 'time_spent', self.gf('django.db.models.fields.DecimalField')(default=0, max_digits=5, decimal_places=2))
+
+        # Changing field 'Task.reject_reason'
+        db.alter_column('projects_task', 'reject_reason', self.gf('django.db.models.fields.CharField')(default='', max_length=64))
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'projects.authorization': {
+            'Meta': {'unique_together': "(('employee', 'project'),)", 'object_name': 'Authorization'},
+            'employee': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'authorization_employee'", 'to': "orm['users.Employee']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'profile': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['projects.Profile']"}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['projects.Project']"})
+        },
+        'projects.deliverable': {
+            'Meta': {'ordering': "['project', 'service', 'name']", 'object_name': 'Deliverable'},
+            'acceptance_criteria': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'approved_by_service_owner': ('django.db.models.fields.CharField', [], {'default': "'P'", 'max_length': '1'}),
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '32', 'null': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['projects.Project']"}),
+            'service': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['services.Service']", 'null': 'True', 'blank': 'True'}),
+            'turnover': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '12', 'decimal_places': '2', 'blank': 'True'}),
+            'unit_time': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'})
+        },
+        'projects.deliverablevolume': {
+            'Meta': {'ordering': "['deliverable', 'date_start']", 'object_name': 'DeliverableVolume'},
+            'date_end': ('django.db.models.fields.DateField', [], {}),
+            'date_start': ('django.db.models.fields.DateField', [], {}),
+            'deliverable': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['projects.Deliverable']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'quantity': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'unit_price': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '6', 'decimal_places': '2', 'blank': 'True'})
+        },
+        'projects.profile': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Profile'},
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.CharField', [], {'max_length': '1', 'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '32'})
+        },
+        'projects.project': {
+            'Meta': {'ordering': "['number', 'name']", 'object_name': 'Project'},
+            'customer_name': ('django.db.models.fields.CharField', [], {'max_length': '128', 'null': 'True', 'blank': 'True'}),
+            'customer_siglum': ('django.db.models.fields.CharField', [], {'max_length': '16', 'null': 'True', 'blank': 'True'}),
+            'date_end': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'date_start': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'department': ('django.db.models.fields.CharField', [], {'max_length': '2', 'null': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'natco': ('django.db.models.fields.CharField', [], {'max_length': '2', 'null': 'True', 'blank': 'True'}),
+            'number': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '32'}),
+            'project_leader': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'project_leader'", 'null': 'True', 'to': "orm['users.Employee']"}),
+            'wiki_link': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
+        },
+        'projects.subject': {
+            'Meta': {'object_name': 'Subject'},
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'project': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['projects.Project']", 'symmetrical': 'False'}),
+            'subject_family': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['projects.SubjectFamily']"})
+        },
+        'projects.subjectfamily': {
+            'Meta': {'object_name': 'SubjectFamily'},
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'})
+        },
+        'projects.task': {
+            'Meta': {'object_name': 'Task'},
+            'answer': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'close_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'creator'", 'to': "orm['users.Employee']"}),
+            'criticity': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'deliverable': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['projects.Deliverable']"}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'number_of_units': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
+            'open_date': ('django.db.models.fields.DateField', [], {}),
+            'owner': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'owner'", 'null': 'True', 'symmetrical': 'False', 'to': "orm['users.Employee']"}),
+            'reject_reason': ('django.db.models.fields.CharField', [], {'max_length': '64', 'null': 'True', 'blank': 'True'}),
+            'requestor': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'O'", 'max_length': '1'}),
+            'subject': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['projects.Subject']", 'symmetrical': 'False'}),
+            'time_spent': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '5', 'decimal_places': '2', 'blank': 'True'})
+        },
+        'services.domain': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Domain'},
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['users.Employee']"})
+        },
+        'services.service': {
+            'Meta': {'ordering': "['service_family__name', 'name']", 'object_name': 'Service'},
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '128'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['users.Employee']", 'null': 'True', 'blank': 'True'}),
+            'service_family': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['services.ServiceFamily']"})
+        },
+        'services.servicefamily': {
+            'Meta': {'ordering': "['domain', 'name']", 'object_name': 'ServiceFamily'},
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True'}),
+            'domain': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['services.Domain']"}),
+            'growth_potential': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '2', 'decimal_places': '0', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['users.Employee']", 'null': 'True', 'blank': 'True'}),
+            'service_lifecycle': ('django.db.models.fields.CharField', [], {'max_length': '1', 'null': 'True', 'blank': 'True'}),
+            'service_position': ('django.db.models.fields.CharField', [], {'max_length': '1', 'null': 'True', 'blank': 'True'}),
+            'trend': ('django.db.models.fields.CharField', [], {'max_length': '1', 'null': 'True', 'blank': 'True'})
+        },
+        'users.employee': {
+            'Meta': {'ordering': "['user__last_name']", 'object_name': 'Employee'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'siglum': ('django.db.models.fields.CharField', [], {'max_length': '16'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'unique': 'True', 'null': 'True'})
+        }
+    }
+
+    complete_apps = ['projects']
