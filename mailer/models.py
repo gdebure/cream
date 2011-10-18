be196ab9140da535ba2af61ac04e46fe571af45d
Guillaume de Bure <guillaume.debure@gmail.com>
2011-10-18 07:19:56 +0200

    Fix emails again, nearly complete now...

--- mailer/models.py
+++ mailer/models.py
@@ -3,47 +3,55 @@ from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.contrib.auth.models import User, Group
 
+from reversion.models import Version
 import reversion
 
-from services.models import Domain, Service, ServiceFamily
-from projects.models import Project, Deliverable, DeliverableVolume
+from projects.models import Deliverable
+from users.models import Employee
 
-@receiver(post_save,sender=Domain)
-@receiver(post_save,sender=Service)
-@receiver(post_save,sender=ServiceFamily)
-@receiver(post_save,sender=Project)
-@receiver(post_save,sender=Deliverable)
-@receiver(post_save,sender=DeliverableVolume)
+
+
+@receiver(post_save,sender=Version)
 def send_mail_on_save(sender, **kwargs):
     
     # This is the list of users that will receive the email
     recipients = User.objects.filter(groups__name='Service Catalog Admins').values_list('email',flat=True)
     # Get the object instance
-    instance = kwargs['instance']
-   
+    version = kwargs['instance']
+    
+    user = Employee.objects.get(pk=version.revision.user.id)
+    date_created = version.revision.date_created
+    
     mail_body = "\n"
+    mail_body += "User who made the change: " + str(user) + "\n"
+    mail_body += "Date: " + str(date_created) + "\n"
    
-    print 'New Instance:' + str(kwargs['created'])
-   
-    if kwargs['created']:
-        # This is a new object, so we don't have any history yet...
-        mail_title = "[CREAM] " + str(sender._meta.verbose_name)+ " " + str(instance) + " created"
-        
-        for field in instance._meta.get_all_field_names():
-            mail_body += field + ": " + str(getattr(instance, field,"")) + "\n"
+    if version.type == 0:
+        # This is an object creation...
+        mail_title = "[CREAM] " + str(version.content_type)+ " " + str(version.object_repr) + " created"
+        mail_body += version.object.get_absolute_url() + "\n"
+        for field in version.field_dict:
+            mail_body += field + ": " + str(version.field_dict[field])+ "\n"
     
+    elif version.type == 2:
+        # This is an object deletion
+        mail_title = "[CREAM] " + str(version.content_type) + " " + str(version.object_repr) + " deleted"
+        for field in version.field_dict:
+            mail_body += field + ": " + str(version.field_dict[field])+ "\n"
+    
+        
     else:
-    
-        # Get the latest two version for comparison:
-        instance_versions = reversion.get_for_object(instance)
-        old_version = instance_versions[0]
-        new_version = instance
+        # this is an updated object
+        # Get the old version for comparison:
+        instance_versions = reversion.get_for_object(version.object)
+        old_version = instance_versions[1]
         
-        mail_title = str(sender._meta.verbose_name)+ " " + str(instance) + " updated"
-    
-        for field in old_version.field_dict:
+        mail_title = "[CREAM] " + str(version.content_type)+ " " + str(version.object_repr) + " updated"
+        mail_body += version.object.get_absolute_url() + "\n"
+        
+        for field in version.field_dict:
             old_object_value = old_version.field_dict[field]
-            new_object_value = str(getattr(instance,field,""))
+            new_object_value = version.field_dict[field]
             if old_object_value != new_object_value:
                 mail_body += "\n" + field + ":\n"
                 mail_body += "----------------\n"
@@ -52,8 +60,9 @@ def send_mail_on_save(sender, **kwargs):
         
     
     mail_body += "\n\nThis is an automatically generated email, please do not reply"
-    
-    send_mail(mail_title, mail_body, 'creamrobot@cimpa.com', recipients, fail_silently=False)
+    print mail_title + "\n"
+    print mail_body + "\n"
+    #send_mail(mail_title, mail_body, 'creamrobot@cimpa.com', recipients, fail_silently=False)
     
 
 
