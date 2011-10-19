31dafe65d5aa9e3d468f3c90f4eb0aff9b207fa1
Guillaume DE BURE <guillaume.debure@gmail.com>
2011-10-19 17:58:57 +0200

    Create a currency filter. Does not work yet...

---
+++ projects/templatetags/currency_filter.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+@register.filter()
+def currency(value):
+    
+    symbol = getattr(settings, 'CURRENCY_SYMBOL', '$')
+    thousand_sep = getattr(settings, 'THOUSAND_SEPARATOR', ' ')
+    decimal_sep = getattr(settings, 'DECIMAL_SEPARATOR', '.')
+
+    intstr = str(int(value))
+    f = lambda x, n, acc=[]: f(x[:-n], n, [(x[-n:])]+acc) if x else acc
+    intpart = thousand_sep.join(f(intstr, 3))
+    return "%s%s%s%s" % (intpart, decimal_sep, ("%0.2f" % value)[-2:], symbol)
+    #return "%s%s%s" % (intpart, decimal_sep, ("%0.2f" % value)[-2:])
+    
+currency.is_safe = True    
+    
