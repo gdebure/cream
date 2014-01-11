b2bc29ab039da3d046a5a72c931e4aed68645e38
Guillaume DE BURE <guillaume.debure@gmail.com>
2014-01-11 22:30:35 +0100

    Revert "Do not track datables  in the repository, datatables needs to be installed separately."
    This reverts commit e174fa579afb895af0c741ca4d104eaaf1fe90b8.

---
+++ CREAM/static/datatables/DT_bootstrap.js
@@ -0,0 +1,11 @@
+/* Table initialisation */
+$(document).ready(function() {
+    $('.dataTable').dataTable( {
+        "bPaginate": false,
+        "bLengthChange": false,
+        "bFilter": false,
+        "bSort": true,
+        "bInfo": false,
+        "bAutoWidth": false
+    } );
+} );
