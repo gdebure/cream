c00e2c498d13db811f4cff32e6e8ddf0295fba07
Guillaume DE BURE <guillaume.debure@airbus.com>
2014-01-20 15:30:39 +0100

    Reverting to standard staticfiles until I understand how this is supposed to work...

--- settings.py
+++ settings.py
@@ -60,7 +60,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -77,7 +77,6 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(os.path.dirname(__file__), 'static').replace('\\','/'),
-    os.path.normpath('/server/static'),
 )
 
 # List of finder classes that know how to find static files in
