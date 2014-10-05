27dd103d8d0246eb77b8f4a66b98fee9e8a45213
Guillaume de Bure <guillaume.debure@gmail.com>
2014-10-05 22:57:32 +0200

    Nah, we don't need a specific library for rendering forms in bootstrap3, a simple filter is enough

--- CREAM/settings.py
+++ CREAM/settings.py
@@ -36,9 +36,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    # Use django-bootstrap3
-    'bootstrap3',
-    
     # Use guardian for per-object permissions
     'guardian',
     
--- core/templatetags/extras.py
+++ core/templatetags/extras.py
@@ -9,3 +9,7 @@ def get(dictionary, key):
 @register.filter
 def field_type(field):
     return field.field.__class__.__name__
+
+@register.filter(is_safe=True)
+def css_class(field,css_class):
+    return field.as_widget(attrs={'class': css_class})
