e9d8ab1fd6c486a77c3ec8100e46d98a5b583727
Guillaume de Bure <guillaume.debure@gmail.com>
2015-01-23 00:35:26 +0100

    More charts

--- reports/views.py
+++ reports/views.py
@@ -87,11 +87,78 @@ class ServiceFamilyReportView(DetailView):
     template_name="servicefamily_report.html"
     model = ServiceFamily
     
+    def get_context_data(self, **kwargs):
+        
+        context = super(ServiceFamilyReportView,self).get_context_data(**kwargs)
+        
+        x_data = []
+        y_data = []
+        total_turnover = 0
+        
+        servicefamily = self.object
+        
+        for service in servicefamily.get_services():
+            x_data.append(str(service))
+            y_data.append(float(service.get_turnover()))
+            total_turnover += service.get_turnover()
+            
+        chartdata = {'x': x_data, 'y': y_data}
+        charttype = "pieChart"
+        chartcontainer = 'piechart_container'
+        data = {
+            'charttype': charttype,
+            'chartdata': chartdata,
+            'chartcontainer': chartcontainer,
+            'extra': {
+                'x_is_date': False,
+                'x_axis_format': '',
+                'tag_script_js': True,
+                'jquery_on_ready': False,
+            }
+        }
+        context.update(data)
+        context['total_turnover'] = total_turnover
+        return context
+    
 
 class ServiceReportView(DetailView):
     
     template_name="service_report.html"
     model = Service
+    
+    def get_context_data(self, **kwargs):
+        
+        context = super(ServiceReportView,self).get_context_data(**kwargs)
+        
+        x_data = []
+        y_data = []
+        total_turnover = 0
+        
+        service = self.object
+        
+        for deliverable in service.get_deliverables():
+            x_data.append(str(deliverable))
+            y_data.append(float(deliverable.get_turnover()))
+            total_turnover += deliverable.get_turnover()
+            
+        chartdata = {'x': x_data, 'y': y_data}
+        charttype = "pieChart"
+        chartcontainer = 'piechart_container'
+        data = {
+            'charttype': charttype,
+            'chartdata': chartdata,
+            'chartcontainer': chartcontainer,
+            'extra': {
+                'x_is_date': False,
+                'x_axis_format': '',
+                'tag_script_js': True,
+                'jquery_on_ready': False,
+            }
+        }
+        context.update(data)
+        context['total_turnover'] = total_turnover
+        return context
+
     
 
 class ServicesReportView(ListView):
