8e2104c489eb595bf022b1b4e986031c317ad772
Guillaume de Bure <guillaume.debure@gmail.com>
2015-02-05 00:45:04 +0100

    Better dataTables handling

--- core/static/dataTables.init.js
+++ core/static/dataTables.init.js
@@ -1,9 +1,19 @@
 /* Table initialisation */
 $(document).ready(function() {
+    
     $('.dataTable').dataTable( {
         "bPaginate": false,
         "bLengthChange": false,
         "bFilter": false,
+        "bSort": true,
+        "bInfo": false,
+        "bAutoWidth": false
+    } );
+    
+    $('.dataTableFiltered').dataTable( {
+        "bPaginate": false,
+        "bLengthChange": false,
+        "bFilter": true,
         "bSort": true,
         "bInfo": false,
         "bAutoWidth": false
