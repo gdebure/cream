45b4e223e1a662f19c5509b715d86e196d1f8a83
Guillaume DE BURE <guillaume.debure@gmail.com>
2015-03-05 11:22:31 +0100

    Link EmployeeStatus to Employee

--- users/models.py
+++ users/models.py
@@ -4,15 +4,9 @@ from django import forms
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
-EMPLOYEE_STATUS_CHOICE=(
-    ('I','Intercontract'),
-    ('S','Structure'),
-    ('P','On Project'),
-    )    
-
 class EmployeeStatus(models.Model):
     '''Indicates the status of an Employee'''
-    id = models.IntegerField(primary_key=True)
+    id = models.CharField(max_length=1,primary_key=True)
     name = models.CharField(max_length=32)
     description = models.TextField()
     css_class = models.CharField(max_length=64)
@@ -29,7 +23,7 @@ class Employee (models.Model):
     
     user = models.OneToOneField(User)
     siglum = models.CharField(max_length=16)
-    status = models.CharField(max_length=1,choices=EMPLOYEE_STATUS_CHOICE)
+    status = models.ForeignKey(EmployeeStatus)
     category = models.CharField(max_length=1, null=True, blank=True)
     
     class Meta:
