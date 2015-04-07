8476531d39e4ca33f311d6161d2b071c8fe41076
Guillaume DE BURE <guillaume.debure@gmail.com>
2015-04-07 18:02:37 +0200

    Locations to be ordered on name by default

--- core/models.py
+++ core/models.py
@@ -4,6 +4,10 @@ class Location(models.Model):
     '''A class that lists the possible locations, used in other apps'''
     
     name = models.CharField(max_length=128)
+
+    class Meta:
+        ordering = ['name']
+    
     
     def __unicode__(self):
         return self.name
