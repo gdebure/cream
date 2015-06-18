f18fef61b009006f90f5c253104b16802893cf47
Guillaume DE BURE <guillaume.debure@gmail.com>
2015-06-18 11:25:23 +0200

    Admin for ProjectStatus

--- projects/admin.py
+++ projects/admin.py
@@ -1,6 +1,7 @@
-from projects.models import Project, Deliverable, DeliverableVolume
+from projects.models import ProjectStatus, Project, Deliverable, DeliverableVolume
 from django.contrib import admin
 
+admin.site.register(ProjectStatus)
 admin.site.register(Project)
 admin.site.register(Deliverable)
 admin.site.register(DeliverableVolume)
