1a20bc2683e7f84d2f5d3bdae6b3463b4594124a
Guillaume DE BURE <guillaume.debure@gmail.com>
2015-06-19 13:13:53 +0200

    Create a new "recruitment" app, dedicated to recruiting new people on identified position

---
+++ recruitment/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from qualifications.models import Position, EmployeePositionStatus
+
+class Applicant(models.Model):
+    
+    last_name = models.CharField(max_length=64)
+    first_name = models.CharField(max_length=64)
+    address = models.TextField()
+    phone = models.CharField(max_length=32)
+    email = models.EmailField()
+    first_contact = models.DateField()
+    cv = models.FileField()
+    comments = models.TextField()
+    
+    def __unicode__(self):
+        return unicode(self.first_name) + " " + unicode(self.last_name)
+    
+class ApplicantPosition(models.Model):
+    
+    applicant = models.ForeignKey(Applicant)
+    position = models.ForeignKey(Applicant)
+    status = models.ForeignKey(EmployeePositionStatus)
+    comments = models.TextField()
+    
