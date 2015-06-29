fead21cc2ddc9b43a60219b9bdf08ebcd14064fd
Guillaume DE BURE <guillaume.debure@gmail.com>
2015-06-29 14:05:40 +0200

    Handle RecruitmentMeeting objects in admin

--- recruitment/admin.py
+++ recruitment/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from recruitment.models import Applicant, ApplicantPosition
+from recruitment.models import Applicant, ApplicantPosition, RecruitmentMeetingType, RecruitmentMeetingStatus, RecruitmentMeeting
 
 admin.site.register(Applicant)
 admin.site.register(ApplicantPosition)
+admin.site.register(RecruitmentMeetingType)
+admin.site.register(RecruitmentMeetingStatus)
+admin.site.register(RecruitmentMeeting)
